72e3dcc5955ad79dd221f0cea2d257ce56d39d1c
Georhi Buklaga <buklaga.98@yandex.ru>
2018-09-21 12:54:59 +0300

    without thinking

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+  var min_number=array[0];
+  var ConsecutiveLength=1,Max_Length=1;
+  for (let i = 1; i < array.length; i++) {
+
+    if(array[i] < min_number){
+      min_number=array[i];
+      array[i-1]=array[i]
+    }
+  }
+  for (let i = 0; i < array.length-1; i++) {
+    if(array[i]+1 == array[i+1]){
+      ConsecutiveLength++;
+    }
+    else{
+      if (Max_Length<ConsecutiveLength) {
+        Max_Length=ConsecutiveLength;
+      }
+      ConsecutiveLength=1;
+    }
+  }
+  // if(Max_Length==1){
+  //   Max_Length=0;
+  // }
+  return Max_Length;
 }
